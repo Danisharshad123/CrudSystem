4b3b4d61c3cf0493cdc4bb79fcc39a69eb8a6023
Danish-Arshad <Danisharshad976@gmail.com>
2022-12-06 10:54:35 +0500

    add

--- src/app/studentdashboard/studentdashboard.component.ts
+++ src/app/studentdashboard/studentdashboard.component.ts
@@ -9,46 +9,12 @@ import { studentmodel } from './student.module';
   styleUrls: ['./studentdashboard.component.css']
 })
 export class StudentdashboardComponent {
-  studentvalue: FormGroup =new FormGroup({
-    name: new FormControl(),
-    class: new FormControl(),
-    email: new FormControl(),
-    password: new FormControl(),
-  })
-  studentobj:studentmodel = new studentmodel;
-  studentList:any
-  constructor( private formbuilder:FormBuilder, private api:ApiService ){}
+ 
+  
+  constructor(  ){}
   ngOninit(): void {
 
   }
-  addstudent(){
-this.studentobj.name = this.studentvalue.value.name;
-this.studentobj.class = this.studentvalue.value.class;
-this.studentobj.email = this.studentvalue.value.email;
-this.studentobj.password = this.studentvalue.value.password;
 
-this.api.poststudent(this.studentobj).subscribe({next: (r) =>{
-  console.log(r)
-} ,
-error:(e) => {
-  console.log(e)
-  alert('Error')
-},
-complete : () => {
-  alert("student Reocrd Added")
-  this.getstudet;
-  this.studentvalue.reset();
-}
-
-
-})
-
-
-  }
-  getstudet(){
-    this.api.getstudent().subscribe(res => {
-      this.studentList
-    })
-  }
   
 }
