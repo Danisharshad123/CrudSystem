2f8ad65b50b3873d366ad949bea2e265f386d7df
Danish-Arshad <Danisharshad976@gmail.com>
2022-12-09 15:04:14 +0500

    done

--- src/app/studentdashboard/studentdashboard.component.ts
+++ src/app/studentdashboard/studentdashboard.component.ts
@@ -9,12 +9,48 @@ import { studentmodel } from './student.module';
   styleUrls: ['./studentdashboard.component.css']
 })
 export class StudentdashboardComponent {
- 
-  
-  constructor(  ){}
+[x: string]: any;
+  studentlist:any = []
+  studentobj : studentmodel = new studentmodel;
+  student :FormGroup=  new FormGroup({
+    name: new FormControl(),
+    class: new FormControl(),
+    email : new FormControl(),
+    password: new FormControl()
+  })
+
+  constructor(private api:ApiService  ){}
   ngOninit(): void {
 
   }
+  Addstudent(){
+  this.studentobj.name = this.student.value.name;
+  this.studentobj.class = this.student.value.class;
+  this.studentobj.email = this.student.value.email;
+  this.studentobj.password= this.student.value.password;
+  this.api.poststudent(this.studentobj).subscribe({next : (v) => {
+    console.log(v);
+  },
+  error: (e) => {
+    console.log(e)
+   
+  },
+  complete:() => {
+    console.log("Student Record Updated!")
+    alert("Student Record Updated")
+   
+  }
+   
+  })
+  }
+  getstudent(){
+    this.api.getstudent().subscribe(res => {
+   this.studentlist = res;
+    })
+  }
+
+  }
+
 
   
-}
+
